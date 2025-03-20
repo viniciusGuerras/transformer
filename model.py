05a7019832d209e0647eb966e55181d19fe1554f
Vinicius Guerra <itsviniciusguerra@gmail.com>
2025-03-20 08:55:24 -0300

    basic model structure

--- model.py
+++ model.py
@@ -10,6 +10,10 @@ head_size = 6
 num_heads = 6
 num_embeds = 512
 dropout = 0.1
+vocab_size = 280
+n_layers = 6
+epochs = 5000
+learning_rate = 1e-3
 #-----
 
 # data loading and pre-processing
@@ -29,6 +33,9 @@ print(data_array)
 
 train_data = data_array[:n]
 val_data = data_array[n:]
+
+def get_batch(ids):
+    pass
  
 class HeadAttention(nn.Module):
     def __init__(self, n_embed, head_size):
@@ -76,6 +83,7 @@ class MultiHeadAttention(nn.Module):
 
 class FeedForward(nn.Module):
     def __init__(self, num_embeds):
+        # basic ReLu with linear layers to increase dimensionality and pick important patterns
         self.net = nn.Sequential(
             nn.Linear(num_embeds, num_embeds * 4),
             nn.ReLU(),
@@ -90,12 +98,67 @@ class Block(nn.Module):
         super().__init__()
         head_size = n_embeds//n_heads
         # divide the attention in differents part of the embedding (to pick various types of connections)
+        # communicate
         self.sa = MultiHeadAttention(n_heads, head_size)
+        # computate
         self.ffwd = FeedForward(n_embeds)
         self.ln1 = nn.LayerNorm(n_embeds)
         self.ln2 = nn.LayerNorm(n_embeds)
 
     def forward(self, x):
+        # residual connections
         x = x + self.ln1(self.sa(x))
         x = x + self.ln2(self.ffwd(x))
         return x
+
+class Transformer(nn.Module):
+    def __init__(self):
+        super().__init__(self, num_embeds, context_window)
+        self.token_embedding_table = nn.Embedding(vocab_size, num_embeds)
+        self.pos_embedding_table = nn.Embedding(context_window, num_embeds)
+        self.blocks = nn.Sequential(*[Block(num_embeds, num_heads) for _ in range(n_layers)])
+        self.ln_f = nn.LayerNorm(num_embeds)
+        self.lm_head = nn.Linear(num_embeds, vocab_size)
+
+    def forward(self, ids, targets=None):
+        B, T = ids.shape
+        toke_emb = self.token_embedding_table(ids)
+        #i need to change to sinusoidal later
+        pos_emb = self.pos_embedding_table(torch.arange(T))
+        sum = toke_emb + pos_emb
+        sum = self.blocks(sum)
+        sum = self.ln_f(sum)
+        logits = self.lm_head(sum)
+
+        if targets == None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits, targets)
+
+        return logits, loss
+
+
+
+"""
+model = Transformer()
+
+optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+
+for iter in range(epochs):
+
+    # sample a batch of data
+    xb, yb = get_batch('train')
+
+    if epochs%100 == 0:
+        print(loss)
+
+    # evaluate the loss
+    logits, loss = model(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+
+"""
