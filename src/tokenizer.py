45d54890956d52f9450e0e7998d1bb7db17a5aa9
Vinicius Guerra <itsviniciusguerra@gmail.com>
2025-03-23 23:44:01 -0300

    smaller changes and common experts

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -1,15 +1,22 @@
 import json
 import regex as re
+import pandas as pd
+import numpy as np
+from collections import Counter
+
+#Later i should rewrite this in C or Rust
+#i could batch process the tokenization process
 
 SPECIAL_TOKENS = {
-    '<sos>' : 100,
-    '<eos>' : 100   
+    '<sos>' : 256,
+    '<eos>' : 257   
 }
 
 #split pattern from chatgpt (got from paper of 4)
 SPLIT_PATTERN = r"""'(?i:[sdmt]|ll|ve|re)|[^\r\n\p{L}\p{N}]?+\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]++[\r\n]*|\s*[\r\n]|\s+(?!\S)|\s+"""
 
 class Tokenizer():
+
     def __init__(self):
         self.merges = {}
 
@@ -28,85 +35,72 @@ class Tokenizer():
         except FileNotFoundError:
             print(f"File {filename} not found. Please train the tokenizer first.")
             
-    #pop is costly make another list to store results
     def merge(self, input, pair, mint):
         result = []
         for element in input:
-            if isinstance(element, int):
-                element = [element]
-
+            if len(element) < 2:
+                result.append(element)
+                continue
+            
+            merged = []
             i = 0
-            while len(element) >= 2 and i < (len(element) - 1):
+            
+            while i < len(element) - 1:
                 if (element[i], element[i+1]) == pair:
-                    element[i] = mint
-                    element.pop(i + 1)
-                    
-                i += 1
-            result.append(element)
-
+                    merged.append(mint)
+                    i += 2
+                else:
+                    merged.append(element[i])
+                    i += 1
+            
+            if i == len(element) - 1:
+                merged.append(element[i])
+            
+            result.append(merged)
+        
         return result
         
-    #i probably can optimize the loop
     def pair_frequency(self, input):
-        result = {}
-        for element in input:
-            if isinstance(element, int):
-                element = [element]
-
-            if len(element) < 2:
-                continue
-
-            for i in range(len(element) - 1):
-                if (element[i], element[i+1]) in result:
-                    result[(element[i], element[i+1])] += 1
-                    continue
-                result[(element[i], element[i+1])] = 1
-
-        if len(result) == 0:
-            return None
-
-        return max(result, key=result.get)  
-            
-    def flatten(self, list):
-        return [item for sublist in list for item in sublist]
+        pairs = (pair for element in input if len(element) > 1 for pair in zip(element, element[1:]))
+        result = Counter(pairs)
+        return max(result, key=result.get) if result else None
 
     def encode(self, text, total_vocab_size):
-        text = "".join([str(x) if not isinstance(x, str) else x for x in text])
-        text = self.split(text)
-        conversion = []
+        text = [['<sos>'] + self.split(phrase) + ['<eos>'] for phrase in text]
+        text = [item for sublist in text for item in sublist]
 
+        conversion = []
         for word in text:
             decoded_line = []
-            for letter in word:
-                byte_letter = ord(letter)
-                if(byte_letter > total_vocab_size):
-                    continue
-                decoded_line.append(byte_letter)
+            print(word)
+            if word in SPECIAL_TOKENS:
+                decoded_line.append(SPECIAL_TOKENS.get(word))
+            else:
+                for letter in word:
+                    byte_letter = ord(letter)
+                    if(byte_letter > total_vocab_size):
+                        continue
+                    decoded_line.append(byte_letter)
             conversion.append(decoded_line)
-
-        num_merges = total_vocab_size - 256
-
-        if(num_merges <= 0):
-            return self.flatten(conversion)
-
+        
         merges = {}
-        for i in range(num_merges):
+        max_merges= total_vocab_size - 256
+        #range based on ASCII size
+        for i in range(max_merges):
             pair = self.pair_frequency(conversion)
-            if pair != None:
-                new_id = 256 + i
-                conversion = self.merge(conversion, pair, new_id)
-                merges[new_id] = pair
+            if pair == None:
+                break
+            new_id = 256 + len(SPECIAL_TOKENS) + i
+            conversion = self.merge(conversion, pair, new_id)
+            merges[new_id] = pair
 
         self.merges = merges
         self.save_merges()
-        return self.flatten(conversion)
+        return [item for sublist in conversion for item in sublist]
 
     def encoded_breakdown(self, id):
         result = []
-        if isinstance(id, list):
-            stack = id.copy()  
-        else:
-            stack = [int(id)]
+        stack = [int(id)]
 
         while stack:
             current_id = int(stack.pop())
@@ -126,5 +120,9 @@ class Tokenizer():
 
         decoded_text = []
         for token in input:
-            decoded_text.extend([chr(x) for x in  self.encoded_breakdown(token) ])
+
+            if token in SPECIAL_TOKENS.values():
+                decoded_text.append([key for key, value in SPECIAL_TOKENS.items() if value == token][0])
+            else:
+                decoded_text.extend([chr(x) for x in  self.encoded_breakdown(token) ])
         return ''.join(decoded_text)
